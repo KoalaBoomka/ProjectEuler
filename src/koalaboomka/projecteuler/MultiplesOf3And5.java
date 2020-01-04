16e25fa225403f81001089c7a4b2284d10c9aaff
Dasha <amynul@gmail.com>
2020-01-04 10:10:53 +0100

    Initial commit

---
+++ src/koalaboomka/projecteuler/MultiplesOf3And5.java
@@ -0,0 +1,17 @@
+package koalaboomka.projecteuler;
+
+public class MultiplesOf3And5 {
+    public static void main(String[] args) {
+        System.out.println("The sum of all the multiples of 3 or 5 below the provided parameter is: " + multiplesOf3and5(1000));
+    }
+
+    public static int multiplesOf3and5(int number) {
+        int sum = 0;
+        for (int i = 0; i < number; i++) {
+            if (i%3 == 0 || i%5 == 0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+}
